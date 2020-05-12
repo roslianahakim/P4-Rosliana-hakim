be0c9c4ec56a2b6e255bd7847c56a03559b44769
roslianahakim <64623032+roslianahakim@users.noreply.github.com>
2020-05-12 00:50:33 -0700

    Create jobsheet 8 C++.cpp
    Mengisi dan Menampilkan Elemen Array yang menggunakan perulangan

---
+++ jobsheet 8 C++.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+int x[8],i;
+cout<<"masukkan NIM terakhir Anda"<<endl;
+for(i=0;i<=4;i++)
+{
+cout<<"Masukkan nim "<<i+1<<" : ";
+cin>>x[i];
+}
+cout<<endl;
+for(i=0;i<=4;i++)
+{
+cout<<"nim ke- "<<i+1<<" = "<<x[i]<<endl;
+} cout<<endl;
+
+Return
