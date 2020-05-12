fa26996a6e22691d3f5167bec84ae2e40ab87cee
roslianahakim <64623032+roslianahakim@users.noreply.github.com>
2020-05-12 00:31:55 -0700

    Create jobsheet 2 C.c
    Mengisi dan Menampilkan Elemen Aray(Dinamis)

---
+++ jobsheet 2 C.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+main()
+{
+int x [5];
+printf("masukan 2 angka akhiran nim anda\n\n");
+printf("nim nilai 1: ");
+scanf("%i",&x[0]);
+printf("nim nilai 2: ");
+scanf("%i",&x[1]);
+printf("nim nilai 3: ");
+scanf("%i",&x[2]);
+scanf("%i",&x[4]);
+printf("\n");
+printf("nim ke-1 =%i\n",x[0]);
+printf("nim ke-2 =%i\n",x[1]);
+printf("nim ke-3 =%i\n",x[2]);
+return 0;
+}
