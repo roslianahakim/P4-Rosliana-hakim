c7e9830d1b7d31ad5e9e342a7b38d176bfaaf747
roslianahakim <64623032+roslianahakim@users.noreply.github.com>
2020-05-12 00:30:55 -0700

    Create jobsheet 1 C++.cpp
    menampilkan array satu dimensi dengan menampilkan NIM masing-masing mahasiswa

---
+++ jobsheet 1 C++.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	
+int nim[5]= {8,2,3,4,5};
+	
+	cout << "Nim ke-1 adalah " << nim[0] << endl;
+	cout << "Nim ke-2 adalah " << nim[1] << endl;
+	cout << "Nim ke-3 adalah " << nim[2] << endl;
+	cout << "Nim ke-4 adalah " << nim[3] << endl;
+	cout << "Nim ke-5 adalah " << nim[4] << endl;
+
+}
