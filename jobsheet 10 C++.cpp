163c3346888848eefa600ab8e033579f1dbd7b5d
roslianahakim <64623032+roslianahakim@users.noreply.github.com>
2020-05-12 00:56:47 -0700

    Create jobsheet 10 C++.cpp
    matriks array tiga dimensi dengan menggunakan NIM tiga orang mahasiswa

---
+++ jobsheet 10 C++.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+main (){
+char nama [2][10][8];
+int i, j;
+for (i = 0; i< 2; i++){
+for (j = 1; j < 10; j++){
+cout<< "nama my best friend  [" <<i<<"][" << j << "] = ";
+cin>>nama[i][j];
+}
+cout<<endl;
+}
+}
