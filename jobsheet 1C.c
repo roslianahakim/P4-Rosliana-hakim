5fab3178f805f5cad8e4140f1a7566be75d2fda3
roslianahakim <64623032+roslianahakim@users.noreply.github.com>
2020-05-12 00:29:44 -0700

    Create jobsheet 1C.c
    Menampilkan array satu dimensi dengan menampilkan NIM masing-masing mahasiswa

---
+++ jobsheet 1C.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+main (){
+int nim[10]= {8, 2, 3, 4, 5};
+
+printf ("Nim ke-1 = %i\n",nim[0]);
+printf ("Nim ke-2 = %i\n",nim[1]);
+printf ("Nim ke-3 = %i\n",nim[2]);
+printf ("Nim ke-4 = %i\n",nim[3]);
+printf ("Nim ke-5 = %i\n",nim[4]);
+
+}
